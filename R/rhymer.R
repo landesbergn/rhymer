ed6cdea2e915c6fc6a15a69a3221488963dbaa74
landesbergn <noah.landesberg@gmail.com>
2017-11-11 15:11:36 -0500

    begin documentation

--- R/rhymer.R
+++ R/rhymer.R
@@ -3,7 +3,16 @@ library(jsonlite)
 
 ua <- user_agent("http://github.com/nlandesberg/rhymer")
 
-datamuse_apidevtools::document() <- function(path) {
+
+#' Call datamuse API and return data frame.
+#'
+#' @param path path to append to datamuse API endpoint.
+#' @return dataframe with data returned from API call.
+#' @export
+#' @examples
+#' datamuse_api("/words?rel_rhy=test")
+#' datamuse_api("/words?ml=test")
+datamuse_api <- function(path) {
   url <- modify_url("https://api.datamuse.com", path = path)
 
   resp <- GET(url, ua)
@@ -35,6 +44,13 @@ datamuse_apidevtools::document() <- function(path) {
   )
 }
 
+#' Print method for data returned by API.
+#'
+#' @param x datamuse API return object.
+#' @export
+#' @examples
+#' print(datamuse_api("/words?rel_rhy=test"))
+#' print(datamuse_api("/words?ml=test"))
 print.datamuse_api <- function(x, ...) {
   cat("<datamuse ", x$path, ">\n", sep = "")
   str(x$content)
@@ -71,11 +87,5 @@ get_spelled_like <- function(word = "test", limit = 10) {
 get_other_related <- function(word = "test", code = "jja", limit = 10) {
   get_data(paste0("/words?rel_", code, "=", word), limit)
 }
-
-get_rhyme("forge")
-get_means_like("forge")
-get_sounds_like("forge")
-get_spelled_like("forge")
-get_other_related("forge", code = "jja")
 
 
