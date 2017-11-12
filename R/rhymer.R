ed57ec7b31eb82ee46a73ea01032d56dbde7effb
landesbergn <noah.landesberg@gmail.com>
2017-11-12 17:02:50 -0500

    make better limiting and error handling

--- R/rhymer.R
+++ R/rhymer.R
@@ -7,12 +7,19 @@ ua <- user_agent("http://github.com/nlandesberg/rhymer")
 #' Call datamuse API and return data frame.
 #'
 #' @param path path to append to datamuse API endpoint.
+#' @param limit number of results to limit the API response to.
 #' @return dataframe with data returned from API call.
 #' @export
 #' @examples
 #' datamuse_api("/words?rel_rhy=test")
 #' datamuse_api("/words?ml=test")
-datamuse_api <- function(path) {
+datamuse_api <- function(path, limit = 10) {
+
+  if (limit > 0) {
+    limit_string <- paste0("&max=", limit)
+    path <- paste0(path, limit_string)
+  }
+
   url <- modify_url("https://api.datamuse.com", path = path)
 
   resp <- GET(url, ua)
@@ -57,35 +64,110 @@ print.datamuse_api <- function(x, ...) {
   invisible(x)
 }
 
-get_data <- function(full_path, limit = 10) {
-  x <- datamuse_api(full_path)
-  content <- x$content
-
-  if ("score" %in% colnames(content)) {
-    content <- content %>% head(limit)
+#' Extract content dataframe from datamuse API call.
+#'
+#' @param full_path API path to append to datamuse API endpoint.
+#' @param limit max number of rows to return from the content dataframe.
+#' @return dataframe with content returned from API call.
+#' @export
+#' @examples
+#' get_content("/words?rel_rhy=test", limit = 5)
+#' get_content("/words?ml=test", limit = 20)
+get_content <- function(full_path, limit = 10) {
+  x <- datamuse_api(full_path, limit)
+  if (length(x$content) == 0) {
+    warning("No results found", call. = FALSE, immediate. = TRUE)
   }
-
-  return(content)
+  return(x$content)
 }
 
-get_rhyme <- function(word = "test", limit = 10) {
-  get_data(paste0("/words?rel_rhy=", word), limit)
+
+#' Get dataframe of rhyming words.
+#'
+#' @param word to rhyme with.
+#' @param limit max number of words to return\.
+#' @return dataframe with rhyming words.
+#' @export
+#' @examples
+#' get_rhyme("test")
+#' get_rhyme("test", limit = 10)
+get_rhyme <- function(word, limit = 10) {
+  get_content(paste0("/words?rel_rhy=", word), limit)
 }
 
+#' Get dataframe of words with similar meaning.
+#'
+#' @param word to get similar meaning with.
+#' @param limit max number of words to return.
+#' @return dataframe with words of similar meaning.
+#' @export
+#' @examples
+#' get_means_like("test")
+#' get_means_like("test", limit = 10)
 get_means_like <- function(word = "test", limit = 10) {
-  get_data(paste0("/words?ml=", word), limit)
+  get_content(paste0("/words?ml=", word), limit)
 }
 
+#' Get dataframe of words that sound similar.
+#'
+#' @param word to get similar sounding words with.
+#' @param limit max number of words to return.
+#' @return dataframe with words that sound similar.
+#' @export
+#' @examples
+#' get_sounds_like("test")
+#' get_sounds_like("test", limit = 10)
 get_sounds_like <- function(word = "test", limit = 10) {
-  get_data(paste0("/words?sl=", word), limit)
+  get_content(paste0("/words?sl=", word), limit)
 }
 
+#' Get dataframe of words that are spelled similarly.
+#'
+#' @param word to get similarly spelled words with.
+#' @param limit max number of words to return.
+#' @return dataframe with words that are spelled similarly.
+#' @export
+#' @examples
+#' get_spelled_like("test")
+#' get_spelled_like("test", limit = 10)
 get_spelled_like <- function(word = "test", limit = 10) {
-  get_data(paste0("/words?sp=", word), limit)
+  get_content(paste0("/words?sp=", word), limit)
 }
 
+#' Get dataframe of words that are related according to a supplied code.
+#'
+#' @param word to get similarly spelled words with.
+#' @param code related word code from http://www.datamuse.com/api/.
+#' @param limit max number of words to return.
+#' @return dataframe with words that are related.
+#' @export
+#' @examples
+#' get_other_related("test", code = "jja", limit = 3)
+#' get_other_related("test", code = "cns", limit = 10)
 get_other_related <- function(word = "test", code = "jja", limit = 10) {
-  get_data(paste0("/words?rel_", code, "=", word), limit)
+  if (code %in% c(
+      "jja",	# Popular nouns modified by the given adjective, per Google Books Ngrams	gradual → increase
+      "jjb",	# Popular adjectives used to modify the given noun, per Google Books Ngrams	beach → sandy
+      "syn",	# Synonyms (words contained within the same WordNet synset)	ocean → sea
+      "trg",	# "Triggers" (words that are statistically associated with the query word in the same piece of text.)	cow → milking
+      "ant",	# Antonyms (per WordNet)	late → early
+      "spc",	# "Kind of" (direct hypernyms, per WordNet)	gondola → boat
+      "gen",	# "More general than" (direct hyponyms, per WordNet)	boat → gondola
+      "com",	# "Comprises" (direct holonyms, per WordNet)	car → accelerator
+      "par",	# "Part of" (direct meronyms, per WordNet)	trunk → tree
+      "bga",	# Frequent followers (w′ such that P(w′|w) ≥ 0.001, per Google Books Ngrams)	wreak → havoc
+      "bgb",	# Frequent predecessors (w′ such that P(w|w′) ≥ 0.001, per Google Books Ngrams)	havoc → wreak
+      "rhy",	# Rhymes ("perfect" rhymes, per RhymeZone)	spade → aid
+      "nry",	# Approximate rhymes (per RhymeZone)	forest → chorus
+      "hom",	# Homophones (sound-alike words)	course → coarse
+      "cns"   # Consonant match	sample → simple
+    )
+  ) {
+    get_content(paste0("/words?rel_", code, "=", word), limit)
+  } else {
+    warning(paste0('"', code, '" is not a valid code. Please refer to the list of valid codes at http://www.datamuse.com/api/'),
+            immediate. = TRUE)
+  }
 }
 
 
